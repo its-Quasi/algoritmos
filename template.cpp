2b934edf624f9153436ebc2cab153061252cfd37
Felipe Alferez <alferezfelipe@gmail.com>
2022-10-18 11:04:01 -0500

    plantilla actualizada

---
+++ template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define endl '\n'
+#define F first
+#define S second
+#define pb push_back
+#define sz(x) int((x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define a(i) _a[1+(i)]
+typedef long long ll;
+typedef pair<int, int> pii;
+ 
+
+int main(){
+  fastIO;
+  int t = 1, n; // test cases
+  //cin>>t
+  while(t--){
+    //Your code here
+  }
+}
